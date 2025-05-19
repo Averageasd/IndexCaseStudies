49b49f768753a1477daf411734f3579ece022ee4
Averageasd <nhuynh9497@gmail.com>
2025-05-18 22:21:25 -0400

    added comments for understanding.

--- AllScripts/Section1-Index Concepts/1 - Index Creation using T-SQL Commands.sql
+++ AllScripts/Section1-Index Concepts/1 - Index Creation using T-SQL Commands.sql
@@ -1,18 +1,24 @@
 use tempdb
 go
 
+--by default, id as primary key is created as clustered index.
 create table product
 (id int primary key,
 pname varchar(50) ,
 productdatetime datetime)
 go
 
+--for the object with id product (table product)
+--find all indexes
 select * from sys.indexes
 where object_name(object_id) = 'product'
 go
 
 -- Creating Non Clustered Index. Following is the Syntax
 --create index IX_TableName_Col1_Col2 on TableName(Col1 Asc, Col2 Desc) 
+--create index -> create non-clustered index
+--too many non-clustered indexes are not good.
+--we will have to update/delete/insert data from/to non-clustered indexes
 create index IX_product_productdatetime on product(productdatetime)
 
 select * from sys.indexes
@@ -28,6 +34,8 @@ pname varchar(50) ,
 productdatetime datetime)
 go
 
+--create a new clustered index 
+--on column id of table product
 alter table product
 add constraint IX_product_id
 PRIMARY KEY CLUSTERED 
